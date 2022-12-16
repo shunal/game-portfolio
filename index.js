e8a1d014b0b6b5a260fdf663e2e110c9914ea2e1
Steven Hunal <stevehunal@gmail.com>
2022-12-16 12:51:55 -0500

    added platforms

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import platform from './img/platform.png'
+console.log(platform)
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 console.log(c)
@@ -5,7 +7,7 @@ console.log(c)
 //set canvas to fullscreen
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const gravity = 0.7 
+const gravity = 0.3 
 //define Player
 class Player {
     constructor() {
@@ -27,6 +29,7 @@ class Player {
     }
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
         
         //GRAVITY IF ELSE STATEMENT (stops the player at bottom of canvas)
@@ -40,15 +43,86 @@ class Player {
 
 }
 
-const player = new Player()
+class Platform {
+    constructor({x,y}) {
+        this.position  = {
+            x: x,
+            y: y
+        }
+        this.width = 200
+        this.height = 20
+    }
+        draw() {
+            c.fillStyle = 'blue'
+            c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        }
+    
+}
 
-// create animation loop
-// clear the canvas
-//call the draw function (update) to maintain Player shape
+const player = new Player()
+const platforms = [
+    new Platform({x:200, y:600}), 
+    new Platform({x: 500, y: 600})]
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+
+}
+
+let scrollOffset = 0
+
+// create loop
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0,canvas.width,canvas.height)
     player.update()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+
+    // stop player at left and middle of screen so platforms can scroll
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.x = 5
+    }
+    else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5
+    }
+
+    else {
+        player.velocity.x = 0
+
+        // platform scrolling logic
+        if (keys.right.pressed) {
+            scrollOffset += 5
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+        }
+        else if (keys.left.pressed) {
+            platforms.forEach(platform => {
+                scrollOffset -= 5
+                platform.position.x += 5
+            })
+        }
+    }
+
+    //platform collision detection
+    platforms.forEach(platform => {
+        platform.draw()
+    
+        if(player.position.y + player.height<= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x<= platform.position.x + platform.width ) {
+         player.velocity.y = 0
+        }
+    })
+    // win condition
+    if (scrollOffset > 2000) {
+        alert("you win")
+    }
 }
 animate()
 
@@ -57,15 +131,41 @@ window.addEventListener('keydown', ({keyCode}) => {
     switch(keyCode) {
         case 87:
             console.log('up')
+            player.velocity.y -= 10
             break
         case 65:
             console.log('left')
+            keys.left.pressed = true
             break
         case 83:
             console.log('down')
+            player.velocity.y += 10
+
             break
         case 68:
             console.log('right')
+            keys.right.pressed = true
+            break
+    }
+})
+window.addEventListener('keyup', ({keyCode}) => {
+    switch(keyCode) {
+        case 87:
+            console.log('up')
+            // player.velocity.y -= 20
+            break
+        case 65:
+            console.log('left')
+            keys.left.pressed = false
+            break
+        case 83:
+            console.log('down')
+            // player.velocity.y += 20
+
+            break
+        case 68:
+            console.log('right')
+            keys.right.pressed = false
             break
     }
 })
